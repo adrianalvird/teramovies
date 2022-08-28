77066265eb8c701d1c95c8b58c587d7a6f7a4d8d
Adrian Alvird <51330833+adrianalvird@users.noreply.github.com>
2022-08-28 22:11:38 +0530

    wsgi add
    from whitenoise.django import DjangoWhiteNoise
    application = DjangoWhiteNoise(application)

--- teravideos/wsgi.py
+++ teravideos/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teravideos.settings')
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
 
 application = get_wsgi_application()
